b806b148d45d6506a62a567634633dcfba5d12ee
Pamhie <pamelaanang327@gmail.com>
2022-04-08 11:02:34 -0700

    New commits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main code
+ * Description: Print state of a random number as
+ * positive, negative, or zero
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	putchar('\n');
+
+	return (0);
+}
