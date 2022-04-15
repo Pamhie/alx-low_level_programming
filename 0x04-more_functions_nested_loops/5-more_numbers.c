b9ffbc1d4a3b79eae3df3bdf4e0104293dd6751b
Pamhie <pamelaanang327@gmail.com>
2022-04-14 19:07:40 -0700

    4&5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* main - print more numbers
+* @void numbers from 0 to 9
+* followed by new line
+*/
+
+void more_numbers(void)
+{
+	int c, i;
+
+	c = 0;
+	while (c < 10)
+	{
+		i = 0;
+		while (i < 15)
+		{
+			if (i > 9)
+				_putchar(i / 10 + '0');
+
+			_putchar(i % 10 + '0');
+			i++;
+		}
+		c++;
+		_putchar('\n');
+	}
+}
+
