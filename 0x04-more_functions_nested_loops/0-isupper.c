9f42e04a03ad6166bc4b7808e9b6a1115e680e0d
Pamhie <pamelaanang327@gmail.com>
2022-04-14 18:44:07 -0700

    0upper again

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,11 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
 * Return 1 otherwise 0
-* 1 if uppercase
-* 0 if lowercase
+*
+* 1 if uppercase and 0 if lowercase
+*
 * @c: the character to be checked
 */
 
