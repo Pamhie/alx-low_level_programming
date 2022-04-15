7d054549666465117402f3be163847404f836bb1
Pamhie <pamelaanang327@gmail.com>
2022-04-14 19:38:10 -0700

    10,11,12

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+
+/**
+* main - largest prime factor of the number
+* Return: 0
+* followed by new line
+*/
+
+int main(void)
+{
+	int c;
+	long num = 612852475143;
+
+	for (c = (int) sqrt(num); c > 2; c++)
+	{
+		if (num % c == 0)
+		{
+			printf("%d\n", c);
+			break;
+		}
+	}
+
+	return (0);
+}
